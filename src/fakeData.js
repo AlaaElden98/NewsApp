9f2002c273bdd2b028cecce2a92bd55a7e04ad59
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-11-04 00:16:22 +0200

    [fakeData] Similiar objects that API response send
    This is for seeing how the UI will look like, once we are satisfied with the UI, start using the real data from the API, and delete this file.# Please enter the commit message for your changes. Lines starting

---
+++ src/fakeData.js
@@ -0,0 +1,256 @@
+// check https://newsapi.org/docs/endpoints/top-headlines
+export const topHeadlineFakeResponse = {
+  status: 'ok',
+  totalResults: 8,
+  articles: [
+    {
+      source: {
+        id: 'the-washington-post',
+        name: 'The Washington Post',
+      },
+      author: 'Mike DeBonis',
+      title:
+        'Senate Republicans block debate on the John Lewis Voting Rights Advancement Act - The Washington Post',
+      description:
+        'The John Lewis Voting Rights Advancement Act would restore federal election-law review provisions that have been struck down by the Supreme Court in recent years.',
+      url: 'https://www.washingtonpost.com/politics/senate-voting-john-lewis/2021/11/03/de00974e-3cc5-11ec-bfad-8283439871ec_story.html',
+      urlToImage:
+        'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/IKMUGPR4CQI6ZPLP3I3W6RZQJY.jpg&w=1440',
+      publishedAt: '2021-11-03T19:46:52Z',
+      content:
+        'The two prior bills put forth by congressional Democrats sought to impose a broad variety of new federal mandates for how states conduct elections, setting minimum standards for early voting and vote… [+4067 chars]',
+    },
+    {
+      source: {
+        id: 'the-washington-post',
+        name: 'The Washington Post',
+      },
+      author: 'Mike DeBonis',
+      title:
+        'Senate Republicans block debate on the John Lewis Voting Rights Advancement Act - The Washington Post',
+      description:
+        'The John Lewis Voting Rights Advancement Act would restore federal election-law review provisions that have been struck down by the Supreme Court in recent years.',
+      url: 'https://www.washingtonpost.com/politics/senate-voting-john-lewis/2021/11/03/de00974e-3cc5-11ec-bfad-8283439871ec_story.html',
+      urlToImage:
+        'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/IKMUGPR4CQI6ZPLP3I3W6RZQJY.jpg&w=1440',
+      publishedAt: '2021-11-03T19:46:52Z',
+      content:
+        'The two prior bills put forth by congressional Democrats sought to impose a broad variety of new federal mandates for how states conduct elections, setting minimum standards for early voting and vote… [+4067 chars]',
+    },
+    {
+      source: {
+        id: 'the-washington-post',
+        name: 'The Washington Post',
+      },
+      author: 'Mike DeBonis',
+      title:
+        'Senate Republicans block debate on the John Lewis Voting Rights Advancement Act - The Washington Post',
+      description:
+        'The John Lewis Voting Rights Advancement Act would restore federal election-law review provisions that have been struck down by the Supreme Court in recent years.',
+      url: 'https://www.washingtonpost.com/politics/senate-voting-john-lewis/2021/11/03/de00974e-3cc5-11ec-bfad-8283439871ec_story.html',
+      urlToImage:
+        'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/IKMUGPR4CQI6ZPLP3I3W6RZQJY.jpg&w=1440',
+      publishedAt: '2021-11-03T19:46:52Z',
+      content:
+        'The two prior bills put forth by congressional Democrats sought to impose a broad variety of new federal mandates for how states conduct elections, setting minimum standards for early voting and vote… [+4067 chars]',
+    },
+    {
+      source: {
+        id: 'the-washington-post',
+        name: 'The Washington Post',
+      },
+      author: 'Mike DeBonis',
+      title:
+        'Senate Republicans block debate on the John Lewis Voting Rights Advancement Act - The Washington Post',
+      description:
+        'The John Lewis Voting Rights Advancement Act would restore federal election-law review provisions that have been struck down by the Supreme Court in recent years.',
+      url: 'https://www.washingtonpost.com/politics/senate-voting-john-lewis/2021/11/03/de00974e-3cc5-11ec-bfad-8283439871ec_story.html',
+      urlToImage:
+        'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/IKMUGPR4CQI6ZPLP3I3W6RZQJY.jpg&w=1440',
+      publishedAt: '2021-11-03T19:46:52Z',
+      content:
+        'The two prior bills put forth by congressional Democrats sought to impose a broad variety of new federal mandates for how states conduct elections, setting minimum standards for early voting and vote… [+4067 chars]',
+    },
+    {
+      source: {
+        id: 'the-washington-post',
+        name: 'The Washington Post',
+      },
+      author: 'Mike DeBonis',
+      title:
+        'Senate Republicans block debate on the John Lewis Voting Rights Advancement Act - The Washington Post',
+      description:
+        'The John Lewis Voting Rights Advancement Act would restore federal election-law review provisions that have been struck down by the Supreme Court in recent years.',
+      url: 'https://www.washingtonpost.com/politics/senate-voting-john-lewis/2021/11/03/de00974e-3cc5-11ec-bfad-8283439871ec_story.html',
+      urlToImage:
+        'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/IKMUGPR4CQI6ZPLP3I3W6RZQJY.jpg&w=1440',
+      publishedAt: '2021-11-03T19:46:52Z',
+      content:
+        'The two prior bills put forth by congressional Democrats sought to impose a broad variety of new federal mandates for how states conduct elections, setting minimum standards for early voting and vote… [+4067 chars]',
+    },
+    {
+      source: {
+        id: 'the-washington-post',
+        name: 'The Washington Post',
+      },
+      author: 'Mike DeBonis',
+      title:
+        'Senate Republicans block debate on the John Lewis Voting Rights Advancement Act - The Washington Post',
+      description:
+        'The John Lewis Voting Rights Advancement Act would restore federal election-law review provisions that have been struck down by the Supreme Court in recent years.',
+      url: 'https://www.washingtonpost.com/politics/senate-voting-john-lewis/2021/11/03/de00974e-3cc5-11ec-bfad-8283439871ec_story.html',
+      urlToImage:
+        'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/IKMUGPR4CQI6ZPLP3I3W6RZQJY.jpg&w=1440',
+      publishedAt: '2021-11-03T19:46:52Z',
+      content:
+        'The two prior bills put forth by congressional Democrats sought to impose a broad variety of new federal mandates for how states conduct elections, setting minimum standards for early voting and vote… [+4067 chars]',
+    },
+    {
+      source: {
+        id: 'the-washington-post',
+        name: 'The Washington Post',
+      },
+      author: 'Mike DeBonis',
+      title:
+        'Senate Republicans block debate on the John Lewis Voting Rights Advancement Act - The Washington Post',
+      description:
+        'The John Lewis Voting Rights Advancement Act would restore federal election-law review provisions that have been struck down by the Supreme Court in recent years.',
+      url: 'https://www.washingtonpost.com/politics/senate-voting-john-lewis/2021/11/03/de00974e-3cc5-11ec-bfad-8283439871ec_story.html',
+      urlToImage:
+        'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/IKMUGPR4CQI6ZPLP3I3W6RZQJY.jpg&w=1440',
+      publishedAt: '2021-11-03T19:46:52Z',
+      content:
+        'The two prior bills put forth by congressional Democrats sought to impose a broad variety of new federal mandates for how states conduct elections, setting minimum standards for early voting and vote… [+4067 chars]',
+    },
+    {
+      source: {
+        id: 'the-washington-post',
+        name: 'The Washington Post',
+      },
+      author: 'Mike DeBonis',
+      title:
+        'Senate Republicans block debate on the John Lewis Voting Rights Advancement Act - The Washington Post',
+      description:
+        'The John Lewis Voting Rights Advancement Act would restore federal election-law review provisions that have been struck down by the Supreme Court in recent years.',
+      url: 'https://www.washingtonpost.com/politics/senate-voting-john-lewis/2021/11/03/de00974e-3cc5-11ec-bfad-8283439871ec_story.html',
+      urlToImage:
+        'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/IKMUGPR4CQI6ZPLP3I3W6RZQJY.jpg&w=1440',
+      publishedAt: '2021-11-03T19:46:52Z',
+      content:
+        'The two prior bills put forth by congressional Democrats sought to impose a broad variety of new federal mandates for how states conduct elections, setting minimum standards for early voting and vote… [+4067 chars]',
+    },
+  ],
+};
+
+// check https://newsapi.org/docs/endpoints/sources
+
+export const sourcesFakeResponse = {
+  status: 'ok',
+  sources: [
+    {
+      id: 'abc-news',
+      name: 'ABC News',
+      description:
+        'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.',
+      url: 'https://abcnews.go.com',
+      category: 'general',
+      language: 'en',
+      country: 'us',
+    },
+    {
+      id: 'abc-news',
+      name: 'ABC News',
+      description:
+        'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.',
+      url: 'https://abcnews.go.com',
+      category: 'general',
+      language: 'en',
+      country: 'us',
+    },
+    {
+      id: 'abc-news',
+      name: 'ABC News',
+      description:
+        'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.',
+      url: 'https://abcnews.go.com',
+      category: 'general',
+      language: 'en',
+      country: 'us',
+    },
+    {
+      id: 'abc-news',
+      name: 'ABC News',
+      description:
+        'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.',
+      url: 'https://abcnews.go.com',
+      category: 'general',
+      language: 'en',
+      country: 'us',
+    },
+    {
+      id: 'abc-news',
+      name: 'ABC News',
+      description:
+        'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.',
+      url: 'https://abcnews.go.com',
+      category: 'general',
+      language: 'en',
+      country: 'us',
+    },
+  ],
+};
+
+// check https://newsapi.org/docs/endpoints/everything#sources
+export const headlinesFromResource = {
+  status: 'ok',
+  totalResults: 3,
+  articles: [
+    {
+      source: {
+        id: 'abc-news',
+        name: 'ABC News',
+      },
+      author: 'JOSH FUNK AP Business Writer',
+      title: 'Deere warns striking workers not to expect better offer',
+      description:
+        "Deere executives say the company won't return to the bargaining table with striking workers because it won't offer a better contract than one they rejected that included immediate 10% raises",
+      url: 'https://abcnews.go.com/Business/wireStory/deere-warns-employees-expect-strike-continues-80958950',
+      urlToImage:
+        'https://s.abcnews.com/images/US/WireAP_c4ed33a77cdc4459917ebd23e448714a_16x9_992.jpg',
+      publishedAt: '2021-11-03T21:51:31Z',
+      content:
+        "Deere executives said Wednesday that the company wouldn't return to the bargaining table with striking workers because it wouldn't offer a better contract than one they rejected that included immedia… [+4244 chars]",
+    },
+    {
+      source: {
+        id: 'abc-news',
+        name: 'ABC News',
+      },
+      author: 'CARA ANNA and AMANDA SEITZ Associated Press',
+      title: "Facebook removes Ethiopian PM's post for inciting violence",
+      description:
+        'Facebook says it has removed a post by Ethiopia’s prime minister that urged citizens to rise up and “bury” the rival Tigray forces who now threaten the capital as the country’s war reaches the one-year mark',
+      url: 'https://abcnews.go.com/International/wireStory/facebook-removes-ethiopian-pms-post-inciting-violence-80958985',
+      urlToImage:
+        'https://s.abcnews.com/images/International/WireAP_18d8d8914a984eaeaf216ec4574f45ef_16x9_992.jpg',
+      publishedAt: '2021-11-03T21:49:39Z',
+      content:
+        'NAIROBI, Kenya -- Facebook says it has removed a post by Ethiopias prime minister that urged citizens to rise up and bury the rival Tigray forces who now threaten the capital as the countrys war reac… [+3706 chars]',
+    },
+    {
+      source: {
+        id: 'abc-news',
+        name: 'ABC News',
+      },
+      author: 'MIKE STOBBE AP Medical Writer',
+      title: 'Panel: All US adults under 60 should get hepatitis B shots',
+      description:
+        'A federal advisory committee has recommended that all U.S. adults younger than 60 be vaccinated against hepatitis B',
+      url: 'https://abcnews.go.com/Health/wireStory/panel-us-adults-60-hepatitis-shots-80958571',
+      urlToImage: 'null',
+      publishedAt: '2021-11-03T21:33:17Z',
+      content:
+        'NEW YORK -- A government advisory committee on Wednesday recommended that all U.S. adults younger than 60 be vaccinated against hepatitis B, because progress against the liver-damaging disease has st… [+2700 chars]',
+    },
+  ],
+};
