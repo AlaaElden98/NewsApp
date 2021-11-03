075a179585a7919f0ac91897c7b641a49f82d1ab
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-11-03 22:12:12 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView,Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Initial commit</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
