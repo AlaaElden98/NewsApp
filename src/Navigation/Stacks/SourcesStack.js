6bb3fdbdb7c69b9f187b258639244c2d8e02f4ed
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-11-04 21:21:39 +0200

    [Navigation]Add ResourceHeadlines to SourcesStack

--- src/Navigation/Stacks/SourcesStack.js
+++ src/Navigation/Stacks/SourcesStack.js
@@ -2,14 +2,16 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import SourcesScreen from '../../screens/SourcesScreen';
-import DetailsScreen from '../../screens/DetailsScreen';
+import ResourceHeadlinesScreen from '../../screens/ResourceHeadlinesScreen'
+import DetailsScreen from '../../screens/DetailsScreen'
 
 const Stack = createNativeStackNavigator();
 
 export function SourcesStack() {
   return (
-    <Stack.Navigator initialRouteName="SourcesScreen" screenOptions={{headerShown: false}}>
+    <Stack.Navigator initialRouteName="SourcesScreen" >
       <Stack.Screen name="SourcesScreen" component={SourcesScreen} />
+      <Stack.Screen name="ResourceHeadlinesScreen" component={ResourceHeadlinesScreen} />
       <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
     </Stack.Navigator>
   );
