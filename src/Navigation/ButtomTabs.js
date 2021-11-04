933eee90d920835d9fcafdf7fb4258843f570127
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-11-04 05:54:33 +0200

    [ButtomTabs]Use Stacks:TopHeadlins,Sources

--- src/Navigation/ButtomTabs.js
+++ src/Navigation/ButtomTabs.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import TopHeadLinesScreen from '../Screens/TopHeadlinesScreen';
-import SourcesScreen from '../Screens/SourcesScreen';
 import HistoryScreen from '../Screens/HistoryScreen';
+import {TopHeadlinesStack} from './Stacks/TopHeadlinesStack';
+import {SourcesStack} from './Stacks/SourcesStack';
 
 const Tab = createBottomTabNavigator();
 
 export const ButtomTabs = () => {
   return (
-    <Tab.Navigator initialRouteName="TopHeadlines">
+    <Tab.Navigator initialRouteName="TopHeadlinesStack">
       <Tab.Screen
-        name="TopHeadlines"
-        component={TopHeadLinesScreen}
+        name="TopHeadlinesStack"
+        component={TopHeadlinesStack}
         options={{title: 'Top headlines'}}
       />
-      <Tab.Screen name="Sources" component={SourcesScreen} />
+      <Tab.Screen
+        name="SourcesStack"
+        component={SourcesStack}
+        options={{title: 'Sources'}}
+      />
       <Tab.Screen name="History" component={HistoryScreen} />
     </Tab.Navigator>
   );
