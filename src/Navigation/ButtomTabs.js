d114e4cc8642b547708c0c81ac9ee3f767a13509
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-11-04 07:24:17 +0200

    [Buttom tabs] Add icons and option

--- src/Navigation/ButtomTabs.js
+++ src/Navigation/ButtomTabs.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import HistoryScreen from '../Screens/HistoryScreen';
 import {TopHeadlinesStack} from './Stacks/TopHeadlinesStack';
 import {SourcesStack} from './Stacks/SourcesStack';
+import {dodgerBlue, lightDodgerBlue} from '../Utilis/colors';
 
 const Tab = createBottomTabNavigator();
 
 export const ButtomTabs = () => {
   return (
-    <Tab.Navigator initialRouteName="TopHeadlinesStack">
+    <Tab.Navigator
+      initialRouteName="TopHeadlinesStack"
+      screenOptions={{
+        tabBarActiveTintColor: dodgerBlue,
+        tabBarInactiveTintColor: lightDodgerBlue,
+      }}>
       <Tab.Screen
         name="TopHeadlinesStack"
         component={TopHeadlinesStack}
-        options={{title: 'Top headlines'}}
+        options={{
+          title: 'Top headlines',
+          tabBarLabelStyle: {fontSize: 15},
+          tabBarIcon: ({color}) => (
+            <MaterialIcons name="trending-up" size={30} color={color} />
+          ),
+        }}
       />
       <Tab.Screen
         name="SourcesStack"
         component={SourcesStack}
-        options={{title: 'Sources'}}
+        options={{
+          title: 'Sources',
+          tabBarLabelStyle: {fontSize: 15},
+          tabBarIcon: ({color}) => (
+            <MaterialIcons name="source" size={30} color={color} />
+          ),
+        }}
       />
-      <Tab.Screen name="History" component={HistoryScreen} />
+      <Tab.Screen
+        name="History"
+        component={HistoryScreen}
+        options={{
+          title: 'History',
+          tabBarLabelStyle: {fontSize: 15},
+          tabBarIcon: ({color}) => (
+            <MaterialIcons name="history" size={30} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
---
+++ src/utilis/colors.js
@@ -0,0 +1,2 @@
+export const dodgerBlue = '#1e90ff';
+export const lightDodgerBlue = '#a7d4ff';
