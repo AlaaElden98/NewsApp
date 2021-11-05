da872c4173df33711e9a765880fd674cf6fbf986
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-11-06 01:14:16 +0200

    [ResourcesHeadlines]Fetch and show the data from API

--- src/Screens/ResourceHeadlinesScreen.js
+++ src/Screens/ResourceHeadlinesScreen.js
@@ -1,12 +1,77 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity, Image, FlatList} from 'react-native';
 import PropTypes from 'prop-types';
+import {useDispatch, useSelector} from 'react-redux';
+
+import {responsiveHeight, responsiveWidth} from '../utilis/helperFunctions';
+import {
+  getSourcesHeadlines,
+  updateSourcesHeadlinesStatus,
+} from '../redux/sourcesHeadlinesSlice';
+import {MAXIMUM_RESULTS_PAGE} from '../utilis/constants';
+
 
 const ResourceHeadlinesScreen = ({navigation, route}) => {
   const {sourceId} = route.params;
+  const [page, setPage] = useState(1);
+  const dispatch = useDispatch();
+
+  const status = useSelector(state => state.sourcesHeadlines.status);
+  console.log(status);
+
+  useEffect(() => {
+    if (status === 'idle')
+      dispatch(getSourcesHeadlines({source_id: sourceId, page: page}));
+  }, [dispatch, page]);
+
+  const data = useSelector(state => state.sourcesHeadlines.items);
+
+  const getNextPage = () => {
+    dispatch(updateSourcesHeadlinesStatus('idle'));
+    setPage(page + 1);
+  };
+  const stopFetching = () => {
+    dispatch(updateSourcesHeadlinesStatus('stop'));
+  };
+
+  const renderItem = ({item}) => {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('DetailsScreen', {
+            author: item.author,
+            title: item.title,
+            sourceUrl: item.url,
+            urlToImage: item.urlToImage,
+            publishedAt: item.publishedAt,
+            content: item.content,
+          });
+        }}>
+        <View>
+          <Image
+            source={{uri: item.urlToImage}}
+            style={{
+              width: responsiveWidth(30),
+              height: responsiveHeight(25),
+              borderRadius: 15,
+            }}
+          />
+          <Text>`Title: {item.title}`</Text>
+          <Text>`Author: {item.author}`</Text>
+          <Text>`publishedAt: {item.publishedAt}`</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View>
-      <Text>ResourcesHeadline</Text>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        onEndReachedThreshold={0.5}
+        onEndReached={page < MAXIMUM_RESULTS_PAGE ? getNextPage : stopFetching}
+      />
     </View>
   );
 };
