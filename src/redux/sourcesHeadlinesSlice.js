c9b445cb2bc7185adaf8748da4a49fe956c201e0
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-11-06 00:28:39 +0200

    [redux]Create sourcesHeadlinesSlice

---
+++ src/redux/sourcesHeadlinesSlice.js
@@ -0,0 +1,51 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+
+import {getSourcesHeadlinesUrl} from '../api/getUrl';
+
+const initialState = {
+  items: [],
+  status: 'idle',
+  error: null,
+};
+
+export const getSourcesHeadlines = createAsyncThunk(
+  'sourcesHeadlines/getSourcesHeadlines',
+  async ({source_id,page}) => {
+    const url = getSourcesHeadlinesUrl(source_id,page);
+    console.log('SourcesHeadlines',url);
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.articles;
+  },
+);
+
+const sourcesHeadlinesSlice = createSlice({
+  name: 'sourcesHeadlines',
+  initialState,
+  reducers: {
+    updateSourcesHeadlinesStatus: (state, action) => {
+      state.status = action.payload;
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(getSourcesHeadlines.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(getSourcesHeadlines.fulfilled, (state, action) => {
+        state.items =
+          state.items === []
+            ? action.payload
+            : [...state.items, ...action.payload];
+        state.status = 'fulfilled';
+      })
+      .addCase(getSourcesHeadlines.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error;
+        console.log(state.error);
+      });
+  },
+});
+
+export const {updateSourcesHeadlinesStatus} = sourcesHeadlinesSlice.actions;
+export default sourcesHeadlinesSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import {configureStore} from '@reduxjs/toolkit';
 import sourcesReducer from './sourcesSlice';
 import topHeadlinesReducerEG from './topHeadlinesEGSlice';
 import topHeadlinesReducerUAE from './topHeadlinesUAESlice';
+import sourcesHeadlinesReducer from './sourcesHeadlinesSlice';
 
 export default store = configureStore({
   reducer: {
     sources: sourcesReducer,
     topHeadlinesEG: topHeadlinesReducerEG,
     topHeadlinesUAE: topHeadlinesReducerUAE,
+    sourcesHeadlines: sourcesHeadlinesReducer,
   },
 });
